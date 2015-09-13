6ef04ba8a198c31185de805e679ee3221e1474c9
beccaades <rebecca.ades@gmail.com>
2015-09-13 13:27:10 -0400

    added method tests

--- config/environment.rb
+++ config/environment.rb
@@ -4,3 +4,4 @@ require 'pry'
 Bundler.require(:default)
 
 require './lib/sum_square.rb'
+require './lib/oo_sum_square.rb'
--- spec/sum_square_spec.rb
+++ spec/sum_square_spec.rb
@@ -4,3 +4,14 @@ describe '#sum_square' do
   end
 end
 
+describe '#add_sum_of_squares' do
+  it 'adds the sum of the squares under a limit' do
+    expect(add_sum_of_squares(3)).to eq(14)
+  end
+end
+
+describe '#square_sum_of_nums' do
+  it 'squares the sum of a range of numbers under a limit' do
+    expect(square_sum_of_nums(3)).to eq(36)
+  end
+end
