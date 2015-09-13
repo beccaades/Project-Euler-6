d3ff4b0f166f365af4e958e79ad75820015b8e9f
beccaades <rebecca.ades@gmail.com>
2015-09-13 13:36:14 -0400

    oo solution passing

--- lib/oo_sum_square.rb
+++ lib/oo_sum_square.rb
@@ -8,22 +8,26 @@ describe 'SumSquare' do
     it "raises Argument Error when initialized without a limit parameter" do
       expect { invalid_app }.to raise_error(ArgumentError)
   end
+
+  describe '#sum_square' do
+    it 'finds the difference between the sum of the square of the first ten numbers and the square of the sum' do
+      expect(SumSquare.new(10).sum_square).to eq(2640)
+    end
+  end
+
+  describe '#add_sum_of_squares' do
+     it 'adds the sum of the squares under a limit' do
+       expect(SumSquare.new(3).add_sum_of_squares(3)).to eq(14)
+     end
+  end
+
+  describe '#square_sum_of_nums' do
+    it 'squares the sum of a range of numbers under a limit' do
+      expect(SumSquare.new(3).square_sum_of_nums(3)).to eq(36)
+    end
+  end
 end
 
-# describe '#sum_square' do
-#   it 'finds the difference between the sum of the square of the first ten numbers and the square of the sum' do
-#     expect(sum_square(10)).to eq(2640)
-#   end
-# end
 
-# describe '#add_sum_of_squares' do
-#   it 'adds the sum of the squares under a limit' do
-#     expect(add_sum_of_squares(3)).to eq(14)
-#   end
-# end
 
-# describe '#square_sum_of_nums' do
-#   it 'squares the sum of a range of numbers under a limit' do
-#     expect(square_sum_of_nums(3)).to eq(36)
-#   end
-# end
+
--- spec/oo_sum_square_spec.rb
+++ spec/oo_sum_square_spec.rb
@@ -5,4 +5,16 @@ class SumSquare
     @num = num
   end
 
+  def sum_square
+    square_sum_of_nums(self.num) - add_sum_of_squares(self.num)
+  end
+
+  def add_sum_of_squares(number)
+    1.upto(number).map {|i| i ** 2}.reduce(:+)
+  end
+
+  def square_sum_of_nums(number)
+    (1..number).reduce(:+).**2
+  end
+
 end
